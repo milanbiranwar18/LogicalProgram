cee05918825537c67760f46665c9690b4cab14f0
Milan Biranwar <milanbiranwar06@gmail.com>
2022-07-29 18:57:37 +0530

    Program for Main Method

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LogicalProgram;
+
+Console.WriteLine("Hello, World!");
+//PowerOf2.PowerOfTwo();
+//HarmonicNumber.harmonicNumber();
+FlipCoin.Flip_Coin();
+//LeapYear.leap_year();
